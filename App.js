b4df189565b4506ca37ce6cb97d60542cbd7fa5f
Leandra Caravlho <69993702+LeandraCrl@users.noreply.github.com>
2021-11-23 20:33:39 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,78 @@
+import React, { useCallback, useEffect, useState } from "react";
+import { Text, View } from "react-native";
+import { Entypo } from "@expo/vector-icons";
+import * as SplashScreen from "expo-splash-screen";
+import * as Font from "expo-font";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import Rotas from "./Rotas";
+import LoginPageYup from "./src/login/LoginPageYup";
+import RegistroPage from "./src/login/RegistroPage";
+import ConfirmaSQLite from "./src/sqlite_page/ConfirmaSQLite";
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+const Stack = createStackNavigator();
+
+export default function App({ navigation }) {
+  const [appIsReady, setAppIsReady] = useState(false);
+
+  useEffect(() => {
+    async function prepare() {
+      try {
+        // Keep the splash screen visible while we fetch resources
+        await SplashScreen.preventAutoHideAsync();
+        // Pre-load fonts, make any API calls you need to do here
+        // await Font.loadAsync(Entypo.font);
+        // Artificially delay for two seconds to simulate a slow loading
+        // experience. Please remove this if you copy and paste the code!
+        // await new Promise((resolve) => setTimeout(resolve, 200));
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        // Tell the application to render
+        setAppIsReady(true);
+        await SplashScreen.hideAsync();
+      }
+    }
+
+    prepare();
+  }, []);
+
+  // const onLayoutRootView = useCallback(async () => {
+  //   if (appIsReady) {
+  //     // This tells the splash screen to hide immediately! If we call this after
+  //     // `setAppIsReady`, then we may see a blank screen while the app is
+  //     // loading its initial state and rendering its first pixels. So instead,
+  //     // we hide the splash screen once we know the root view has already
+  //     // performed layout.
+  //     await SplashScreen.hideAsync();
+  //   }
+  // }, [appIsReady]);
+
+  // if (!appIsReady) {
+  //   return null;
+  // }
+
+  return (
+    // <View
+    //   style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
+    //   onLayout={onLayoutRootView}
+    // >
+    //   <Text>SplashScreen Demo! 👋</Text>
+    //   <Entypo name="rocket" size={30} />
+
+    // </View>
+    <NavigationContainer independent={true}>
+      <Stack.Navigator initialRouteName="LoginPage">
+        <Stack.Screen name="LoginPage" component={LoginPage} />
+        <Stack.Screen name="RegistroPage" component={RegistroPage} />
+        <Stack.Screen
+          name="Rotas"
+          component={Rotas}
+          options={{ headerShown: false }}
+        />
+        
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
---
+++ Rotas.js
@@ -0,0 +1,35 @@
+import "react-native-gesture-handler";
+import React from "react";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
+import { Text, View, Button } from 'react-native';
+import InicialPage from './src/inicial/InicialPage';
+import NotificacoesPage from './src/notificacoes/NotificacoesPage';
+// import LoginPageYup from "./src/login/LoginPageYup";
+// import RegistroPage from "./src/login/RegistroPage";
+import HookForm from "./src/hook_form/HookForm";
+import CadastroSQLite from "./src/sqlite_page/CadastroSQLite";
+import ConfirmaSQLite from "./src/sqlite_page/ConfirmaSQLite";
+
+const Drawer = createDrawerNavigator();
+const Stack = createStackNavigator();
+
+const Rotas = () => {
+  
+  return (
+    <NavigationContainer  independent={true}>
+      <Drawer.Navigator initialRouteName="InicialPage"> 
+        <Drawer.Screen name="InicialPage" component={InicialPage} />
+        <Drawer.Screen name="NotificacoesPage" component={NotificacoesPage} />
+        {/* <Drawer.Screen name="LoginPageYup" component={LoginPageYup} />
+        <Drawer.Screen name="RegistroPage" component={RegistroPage} /> */}
+        <Drawer.Screen name="HookForm" component={HookForm} />
+        <Drawer.Screen name="CadastroSQLite" component={CadastroSQLite} />
+        <Stack.Screen name="ConfirmaSQLite" component={ConfirmaSQLite} />
+      </Drawer.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default Rotas;
